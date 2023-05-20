0c688885f6366550e91dda3d64c49cf73985e05e
Ayush chalisee <ayushchalise12@gmail.com>
2023-05-20 15:05:22 +0200

    Path Module

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
+const os = require('os')
 
-// sayHi('Susan')
-// sayHi(names.john)
-// sayHi(names.peter)
+//info about current user
+const user = os.userInfo()
 
+
+
+//method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS)
