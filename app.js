c6e70cda920dc94588f61b586de67373cf2f824b
Ayush chalisee <ayushchalise12@gmail.com>
2023-05-22 15:07:40 +0200

    First Package

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const http = require('http');
+const _ = require('lodash')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to our home page');
-    return;
-  }
-  if (req.url === '/about') {
-    res.end('Here is our short history');
-    return;
-  }
-  res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-  `);
-});
+const items = [1, [2,[3, [4]]]]
+const newItems = _.flattenDeep (items)
 
-server.listen(3000);
+console.log(newItems)
